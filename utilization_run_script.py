5e94d64b4e32bb833a65c051d90db7468da5f2ca
Johnny Rhe <57951442+djwhsdj@users.noreply.github.com>
2023-09-17 11:19:44 +0900

    Update utilization_run_script.py

--- utilization_run_script.py
+++ utilization_run_script.py
@@ -10,12 +10,12 @@ mode = int(input())
 
 '''
 original = MODE
-1: Baseline, 4: KERNTROL
+1: Baseline, 2: KERNTROL-C, 4: KERNTROL-M
 ab: acitvation bit precision
 wb: weight bit precision
 l_th: threshold
-ar: array row
-ac: array column
+ar: array row size
+ac: array column size
 The model can be changed with 'ResNet-Q, WRN16-4'
 '''
 
@@ -34,49 +34,6 @@ if mode == 1 : #
     ac = 256
     save = 0
 
-elif mode == 2 : # 
-    original = [4]
-    epoch = [100] 
-    wb = [1]
-    ab = [4]
-    seed_candidate = [1106, 1992]
-    model = 'ResNet_Q'
-    dataset = ['cifar10']
-    GPU = 3
-    l5_th = [0.15, 0.25, 0.4, 0.45]
-    # l4_th = [0.25]
-    ar = [512]
-    ac = 256
-    save = 0
-
-elif mode == 3 : # 
-    original = [4]
-    epoch = [200] 
-    wb = [2]
-    ab = [8]
-    seed_candidate = [1106, 1992]
-    model = 'Wide_ResNet_Q'
-    dataset = ['cifar100']
-    GPU = 2
-    l5_th = [0.15, 0.25, 0.4, 0.45]
-    ar = [2048]
-    ac = 1024
-    save = 0
-
-elif mode == 4 : # 
-    original = [1]
-    epoch = [100] 
-    wb = [1]
-    ab = [4]
-    seed_candidate = [1106, 1992]
-    model = 'ResNet_Q'
-    dataset = ['cifar10']
-    GPU = 0
-    l5_th = [0]
-    # l4_th = [0]
-    ar = [2048]
-    ac = 1024
-    save = 1
 
 #########################################
 
